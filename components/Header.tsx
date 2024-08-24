8b1a54c599f9f14e66d82a6ff0b9984d4892b84a
Muhammad Hamza <mhamza191703@gmail.com>
2024-08-24 12:15:05 +0500

    changing link

--- components/Header.tsx
+++ components/Header.tsx
@@ -44,7 +44,7 @@ const Header = () => {
                 <Button color="inherit" component={Link} href="/Scrapper">
                   Scrapper
                 </Button>
-                <Link href="https://rate-my-professor-backend.vercel.app/"  color="inherit">
+                <Link href="https://rag-bot-lime.vercel.app/"  color="inherit">
                   <Typography variant="body2">Try Our Chatbot 2</Typography>
                 </Link>
               </Box>
